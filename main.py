58db8319d2797ec18c75c13496b5b37b208832ea
sled45 <140016048+sled45@users.noreply.github.com>
2023-10-28 20:25:20 -0400

    changed the initial dialog as it references a bug i fixed

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def ReplaceWithOof(s):
                 CopyFile(resource_path("bruh.ogg"), ouchPath)
                 MessageBox(None, "Success! The previous death sound has been backed up in case you change your mind.", "BringBackTheOof", MB_ICONINFORMATION | MB_OK | MB_DEFBUTTON1)
         
-startMsg = MessageBox(None, "Do you want to replace the death sound?\n(Yes = Roblox OOF, No = bruh sfx)\nMake sure the most recently modified folder in the Roblox Versions folder is the client and not Roblox Studio!", "BringBackTheOof (Coded by @bluesled45 on Roblox)", MB_ICONQUESTION | MB_YESNOCANCEL | MB_DEFBUTTON1)
+startMsg = MessageBox(None, "Do you want to replace the death sound?\n(Yes = Roblox OOF, No = bruh sfx)", "BringBackTheOof (Coded by sled45 on GitHub)", MB_ICONQUESTION | MB_YESNOCANCEL | MB_DEFBUTTON1)
 if startMsg == 6:
     print("epic duck!!!!!!!11!!!")
     ReplaceWithOof(0)
